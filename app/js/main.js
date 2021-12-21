e3dd8b4a908576d281ad1f58e1fed910d139e799
Chopa23 <cholpon.sharshenova.pls@gmail.com>
2021-12-21 13:28:58 -0500

    slider is done

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,11 @@
 $(function () {
-  // $(".banner__slider-img").slick({
-  //   dots: true,
-  //   infinite: true,
-  //   slidesToShow: 1,
-  // });
   $(".banner__slider").slick({
     dots: true,
+    // slidesToShow: 4,
+    // slidesToScroll: 1,
     prevArrow:
-      '<button class="banner-slider-btn banner-slider-btn-prev"><img src="images/file:///C:/Users/User/Documents/Drive-Moto/app/images/left.svg" alt=""></button>',
+      '<button class="banner-slider-btn banner-slider-btn-prev"><img src="images/left.svg" alt=""></button>',
     nextArrow:
       '<button class="banner-slider-btn banner-slider-btn-next"><img src="images/right.svg" alt=""></button>',
   });
-  // $(".products__slider").slick({
-  //   slidesToShow: 4,
-  //   slidesToScroll: 1,
-  //   prevArrow:
-  //     '<button class="banner-slider-btn products__slider-btn-prev banner-slider-btn-prev"><img src="images/arrow-left-dark.svg" alt=""></button>',
-  //   nextArrow:
-  //     '<button class="banner-slider-btn products__slider-btn-next banner-slider-btn-next"><img src="images/arrow-right-dark.svg" alt=""></button>',
-  // });
 });
